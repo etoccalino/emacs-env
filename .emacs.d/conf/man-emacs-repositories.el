9dad1102d8bf6b8ff9499875b27c8a93b259f7d0
Elvio Toccalino <elviotoccalino@yahoo.com>
2012-09-19 01:28:32 -0300

    added MELPA to the repos.

--- .emacs.d/conf/man-emacs-repositories.el
+++ .emacs.d/conf/man-emacs-repositories.el
@@ -1,6 +1,10 @@
-;; Marmalade repository
 (package-initialize)
+
+;; Marmalade repository
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+;; MELPA repository
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 
 ;;
 ;; byte-compile init files.

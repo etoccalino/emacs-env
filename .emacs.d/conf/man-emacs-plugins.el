b71746e607c47d7d1a416d85e725fc2934663f2b
Elvio Toccalino <elviotoccalino@yahoo.com>
2012-09-22 12:04:16 -0300

    eliminated unnecessary load-path additions. package-initialize was doing this for me.

--- .emacs.d/conf/man-emacs-plugins.el
+++ .emacs.d/conf/man-emacs-plugins.el
@@ -3,18 +3,15 @@
 ;; * pyflakes, system
 
 ;; Auto Complete mode
-(add-to-list 'load-path "~/.emacs.d/elpa/auto-complete-1.4/")
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-1.4/ac-dict")
 (ac-config-default)
 
 ;; Ace Jump!
-(add-to-list 'load-path "~/.emacs.d/elpa/ace-jump-mode-1.0")
 (require 'ace-jump-mode)
 (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
 ;; Find Things Fast
-(add-to-list 'load-path "~/.emacs.d/plugins/find-things-fast")
 (require 'find-things-fast)
 (global-set-key (kbd "C-x M-f") 'ftf-find-file)
 (global-set-key (kbd "C-x M-s") 'ftf-grepsource)
@@ -22,9 +19,6 @@
 (add-hook 'python-mode-hook (lambda () (ftf-add-filetypes '("*.html"))))
 
 ;; PyFlakes integration
-(add-to-list 'load-path "~/.emacs.d/elpa/pyflakes-1.0")
-(add-to-list 'load-path "~/.emacs.d/elpa/flymake-cursor-1.0.2")
-(add-to-list 'load-path "~/.emacs.d/elpa/rfringe-1.0.1")
 (when (load "flymake" t)
   (defun flymake-pyflakes-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy

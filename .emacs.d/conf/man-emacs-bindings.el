e7b5f1b890edba220767095c849d46b568c2bcda
etoccalino <elvio.toccalino@cultura.gov.ar>
2012-10-22 16:39:13 -0300

    added a fast comment/uncomment binding.

--- .emacs.d/conf/man-emacs-bindings.el
+++ .emacs.d/conf/man-emacs-bindings.el
@@ -4,6 +4,9 @@
 ;; Move the other window fast
 (global-set-key (kbd "M-1") 'other-window)
 
+;; Comment or uncomment region.
+(global-set-key (kbd "C-c C-j") 'comment-or-uncomment-region)
+
 ;;
 ;; byte-compile init files.
 (add-hook 'after-save-hook 'byte-compile-current-buffer)

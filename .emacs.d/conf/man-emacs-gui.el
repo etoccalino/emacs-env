9d45a8b2128954fc583be3d3f915a9f64bbf352e
etoccalino <elvio.toccalino@cultura.gov.ar>
2012-10-30 13:22:05 -0300

    use y-or-n instead of yes-or-no-plus-ENTER

--- .emacs.d/conf/man-emacs-gui.el
+++ .emacs.d/conf/man-emacs-gui.el
@@ -13,6 +13,9 @@
 (setq fill-column 79)
 (toggle-truncate-lines 1)
 
+;; Use "y or n" insteas of "yes or no + ENTER".
+(fset 'yes-or-no-p 'y-or-n-p)
+
 ;; Depends on the inconsolata font being installed in the system. It's worth it.
 (set-frame-font "inconsolata-11")
 ;; Set the face in all frames, so I can run as daemon.
